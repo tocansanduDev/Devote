138b3a3757f3e7cc2c8bb1fba6fb65199c6345db
Tocan Sandu <tocansandu93@gmail.com>
2022-06-08 08:43:39 +0300

    new Project + git ignore

---
+++ Devote/Devote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Devote
+//
+//  Created by Sandu on 08.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Devote/Devote/DevoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Sandu on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
